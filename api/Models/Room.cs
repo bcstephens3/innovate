885c299701403c7a06d238e7c3b916f6fd9c5655
Andrew Stevenson <andrewstevenson@Andrews-MacBook-Pro-2.local>
2022-04-03 07:02:31 -0500

    staging stuff

--- api/Models/Room.cs
+++ api/Models/Room.cs
@@ -9,6 +9,7 @@ namespace api.Models
         public string Status { get; set; }
         public string Size { get; set; }
         public string Capacity {get; set;}
+        public string FloorNumber {get; set;}
         public string StartTime { get; set; }
         public string EndTime { get; set; }
         public string Deleted { get; set; }
